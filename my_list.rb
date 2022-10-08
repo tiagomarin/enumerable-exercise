f4809c11ef8c240319476c50e7cec8b38c2032b2
Tiago Marin <tiago.lelinski@gmail.com>
2022-10-07 21:14:29 -0300

    add enumerable methods

--- my_list.rb
+++ my_list.rb
@@ -10,4 +10,14 @@ class MyList
   end
 end
 
-testList = MyList.new(1,2,3,4)
+list = MyList.new(1,2,3,4)
+
+
+puts list.all? {|e| e < 5}
+puts list.all? {|e| e > 5}
+
+puts list.any? {|e| e == 2}
+puts list.any? {|e| e == 5}
+
+print list.filter {|e| e.even?}
+
